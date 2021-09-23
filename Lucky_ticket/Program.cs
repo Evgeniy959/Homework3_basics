4979119a2cb8d90440337fc190113377b2dff4a3
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2021-09-23 20:13:52 +0700

    Add project files.

---
+++ Lucky_ticket/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Lucky_ticket
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите номер трамвайного билета (6-значное число)");
+            string str = Console.ReadLine();
+            char[] ch = new char[str.Length];
+            ch = str.ToCharArray();
+            int[] ticketNumber = new int[ch.Length];            
+            ticketNumber = ch.Select(s => int.Parse(s.ToString())).ToArray();            
+            //int[] ticketNumber = ch.Select(s => int.Parse(s.ToString())).ToArray(); // сокращенный
+            if (ch.Length == 6)
+            {
+                int leftNumber = 0;
+                int rightNumber = 0;
+
+                for (int i = 0; i < ch.Length; i++)
+                {
+                    if (i < 3)
+                    {
+                        leftNumber += ticketNumber[i];
+                    }
+
+                    else rightNumber += ticketNumber[i];
+                }
+
+                if (leftNumber == rightNumber)
+                {
+                    Console.WriteLine($"У вас счастливый билет: {leftNumber} = {rightNumber}");
+                }
+
+                else Console.WriteLine($"У вас не счастливый билет: {leftNumber} != {rightNumber}");
+            }
+
+            else Console.WriteLine("Число введено не правильно!");            
+        }
+    }
+}
---
+++ Сount_space/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Сount_space
+{
+    class Program
+    {
+                                  // 1 способ
+        static void Main()
+        {            
+            Console.WriteLine("Введите символы: ");            
+            string str = Console.ReadLine();
+            var counter = СountSpace(str);            
+            Console.WriteLine($"Количество пробелов: {counter}");
+        }
+        static int СountSpace(string str)
+        {
+            var counter = 0;                        
+            foreach (var symb in str)
+            {                              
+                if (symb == ' ')
+                {
+                    ++counter;
+                }
+                if (symb == '.') break;                
+
+            }
+            return counter;
+        }
+//---------------------------------------------------------------------------------------------
+                                    // 2 способ
+        /*static void Main()
+        {
+            char key;
+            int count_space = 0;
+            Console.WriteLine("Введите символы: ");
+            do
+            {
+                
+                key = Console.ReadKey().KeyChar;
+                if (key == ' ')
+                    count_space++;
+            }
+            while (key != '.');
+            Console.WriteLine();           
+            Console.WriteLine($"Количество пробелов: {count_space}");
+        }*/
+    }
+}
